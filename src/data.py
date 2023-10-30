bfc3930239ab685e0329694c9e67506db64c191f
huaminxu <huaminxu3@gmail.com>
2023-10-30 20:14:11 +0800

    dataloader_final_test

--- src/data.py
+++ src/data.py
@@ -397,6 +397,7 @@ if __name__ == "__main__":
     data_loader = MyDataLoader(dataset, batch_size=1,
                                   num_workers=4)
     for i, batch in enumerate(data_loader):
+        print(batch)
         mixtures, lens, labels = batch
         print(i)
         print(mixtures.size())
