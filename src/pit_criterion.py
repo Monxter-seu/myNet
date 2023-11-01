0ccce9bca560a71878736cfd3308a6d2a8925f2d
huaminxu <huaminxu3@gmail.com>
2023-11-01 21:14:19 +0800

    CrossEntropyLoss 'float Bug'

--- src/pit_criterion.py
+++ src/pit_criterion.py
@@ -11,8 +11,6 @@ EPS = 1e-8
 def new_loss(source_label, estimate_label):
 
     print('cal_loss')
-    source_label = source_label.float()
-    estimate_label = estimate_label.float()
     source_first_class = source_label[:, 0, :]
     source_second_class = source_label[:, 1, :]
     estimate_first_class = estimate_label[:, 0]
