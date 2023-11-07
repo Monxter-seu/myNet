2bca50cad1bebfd4e5f734ad393dae2f9d738095
huaminxu <huaminxu3@gmail.com>
2023-11-07 22:30:49 +0800

    net version 2

--- src/pit_criterion.py
+++ src/pit_criterion.py
@@ -12,9 +12,9 @@ def new_loss(source_label, estimate_label):
     #source为网络计算结果，estimate为直接输入
     source_label = source_label.float()
     estimate_label = estimate_label.long()
-    source_first_class = source_label[:, 0].unsqueeze(1)
+    source_first_class = source_label[:, 0]
     source_second_class = source_label[:, 1:6]
-    estimate_first_class = estimate_label[:, 0].unsqueeze(1)
+    estimate_first_class = estimate_label[:, 0].unsqueeze(1).float()
     estimate_second_class = estimate_label[:, 1]
 
     criterion1 = torch.nn.BCELoss()
