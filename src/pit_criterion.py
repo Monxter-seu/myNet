293f26a194c0b0332d40443d0995f7dff70fc542
huaminxu <huaminxu3@gmail.com>
2023-11-07 20:37:54 +0800

    net version 2

--- src/pit_criterion.py
+++ src/pit_criterion.py
@@ -17,8 +17,9 @@ def new_loss(source_label, estimate_label):
     estimate_first_class = estimate_label[:, 0]
     estimate_second_class = estimate_label[:, 1]
 
-    criterion1 = torch.nn.CrossEntropyLoss()
-    criterion2 = torch.nn.BCELoss()
+    criterion1 = torch.nn.BCELoss()
+    criterion2 = torch.nn.CrossEntropyLoss()
+
     loss_first_class = criterion1(source_first_class, estimate_first_class)
     loss_second_class = criterion2(source_second_class, estimate_second_class)
 
