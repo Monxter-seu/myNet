9bb38496a9b4be5dbaecc948666760de20554ffb
Monxter-seu <huaminxu3@gmail.com>
2023-11-07 17:06:49 +0800

    version one final

--- src/test.py
+++ src/test.py
@@ -40,10 +40,10 @@ if __name__ == "__main__":
     # reader = csv.reader(file)
     # row = next(reader)
     # tensor_read = torch.from_numpy(np.array(row, dtype=np.float32))
-    N = 512
-    L = 40
-    B = 512
-    H = 256
+    N = 128
+    L = 20
+    B = 128
+    H = 32
     P = 3
     X = 8
     R = 4
@@ -59,10 +59,14 @@ if __name__ == "__main__":
 
     # 定义损失函数和优化器
     # criterion = new_loss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    train_dataset = MyDataset('D:\\csvProcess\\testout\\tr\\', 4)
-    train_loader = MyDataLoader(train_dataset, batch_size=1)
+    train_dataset = MyDataset('D:\\csvProcess\\testout\\tr\\', batch_size=16)
+    train_loader = MyDataLoader(train_dataset, batch_size=1, shuffle=True)
+    cv_dataset = MyDataset('D:\\csvProcess\\testout\\tt\\', batch_size=16)
+    cv_loader = MyDataLoader(cv_dataset, batch_size=1, shuffle=True)
+    test_dataset = MyDataset('D:\\csvProcess\\testout\\cv\\', batch_size=16)
+    test_loader = MyDataLoader(test_dataset, batch_size=1, shuffle=True)
     # 训练模型
     num_epochs = 30
     for epoch in range(num_epochs):
@@ -102,19 +106,54 @@ if __name__ == "__main__":
             train_correct1 += (predicted1 == labels[:,1]).int().sum().item()
             total_samples += labels.size(0)
             #print('total_samples===',total_samples)
-
+            
         train_loss /= len(train_loader.dataset)
         train_accuracy0 = train_correct0 / total_samples
         train_accuracy1 = train_correct1 / total_samples
         print('============')
         print('train_loss', train_loss)
         print('train_accuracy0', train_accuracy0)
-        print('train_accuracy1', train_accuracy1)
+        print('train_accuracy1', train_accuracy1)  
+        
+        
+        model.eval()
+        cv_loss = 0
+        cv_correct0 = 0
+        cv_correct1 = 0
+        total_samples = 0
+        with torch.no_grad():
+            for data, labels in cv_loader:
+                # 将数据和标签转换为张量
+                data = data.cuda()
+                labels = labels.cuda()
+        
+                # 向前传递
+                outputs = model(data)
+                loss = new_loss(outputs, labels)
+        
+                cv_loss += loss.item() * data.size(0)
+                _, predicted0 = torch.max(outputs[:,0], 1)
+                _, predicted1 = torch.max(outputs[:,1], 1)
+                cv_correct0 += (predicted0 == labels[:,0]).int().sum().item()
+                cv_correct1 += (predicted1 == labels[:,1]).int().sum().item()
+                total_samples += labels.size(0)
+            cv_loss /= len(cv_loader.dataset)
+            cv_accuracy0 = cv_correct0 / total_samples
+            cv_accuracy1 = cv_correct1 / total_samples
+
+            print('cv_loss', cv_loss)
+            print('cv_accuracy0', cv_accuracy0)
+            print('cv_accuracy1', cv_accuracy1)
+            torch.save(model.state_dict(), "Classifier.pth")
+            print('==========')
+
+        
 
     # 测试模式
     model.eval()
     test_loss = 0
-    test_correct = 0
+    test_correct0 = 0
+    test_correct1 = 0
     total_samples = 0
     with torch.no_grad():
         for data, labels in test_loader:
@@ -129,18 +168,18 @@ if __name__ == "__main__":
             test_loss += loss.item() * data.size(0)
             _, predicted0 = torch.max(outputs[:,0], 1)
             _, predicted1 = torch.max(outputs[:,1], 1)
-            train_correct0 += (predicted0 == labels[:,0]).int().sum().item()
-            train_correct1 += (predicted1 == labels[:,1]).int().sum().item()
+            test_correct0 += (predicted0 == labels[:,0]).int().sum().item()
+            test_correct1 += (predicted1 == labels[:,1]).int().sum().item()
             total_samples += labels.size(0)
         test_loss /= len(train_loader.dataset)
-        test_accuracy0 = train_correct0 / total_samples
-        test_accuracy1 = train_correct1 / total_samples
+        test_accuracy0 = test_correct0 / total_samples
+        test_accuracy1 = test_correct1 / total_samples
 
         print('test_loss', test_loss)
         print('test_accuracy0', test_accuracy0)
         print('test_accuracy1', test_accuracy1)
-    torch.save(model.state_dict(), "Classifier.pth")
-    print('==========')
+        torch.save(model.state_dict(), "Classifier.pth")
+        print('==========')
     
     # # 测试模式
     # model.eval()
